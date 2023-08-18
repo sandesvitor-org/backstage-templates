fc675b2abc8342de700f940a348bd51ff9627b12
Vitor Sandes <sandesvitor@gmail.com>
2023-08-18 07:48:13 -0300

    fix

--- create-resources/s3-v2/template/${{ values.bucket_name }}/backend.tf
+++ create-resources/s3-v2/template/${{ values.bucket_name }}/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
     bucket = "my-terraform-bucket"
-    key    = "aws/${{ parameters.account }}/global/s3/${{ values.bucket_name }}/terraform.tfstate"
+    key    = "aws/${{ values.account }}/global/s3/${{ values.bucket_name }}/terraform.tfstate"
   }
 }
--- create-resources/s3-v2/template/${{ values.bucket_name }}/provider.tf
+++ create-resources/s3-v2/template/${{ values.bucket_name }}/provider.tf
@@ -1,6 +1,7 @@
+{% set settings = config.account_settings[parameters.account] %}
 provider "aws" {
   region = "us-east-1"
   assume_role {
-    role_arn = local.account_role_arn
+    role_arn = "{{ settings.role_arn }}"
   }
 }
