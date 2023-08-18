0bbeda75e55e88ac9862a2ba3c2a46359253e903
Vitor Sandes <sandesvitor@gmail.com>
2023-08-18 11:41:44 -0300

    fix

--- create-resources/s3-v2/template/${{ values.bucket_name }}/provider.tf
+++ create-resources/s3-v2/template/${{ values.bucket_name }}/provider.tf
@@ -1,7 +1,16 @@
+{% set roleArn = steps.aws-config.output.result.account_settings[parameters.account].role_arn %}
 provider "aws" {
   region = "us-east-1"
 
   assume_role {
-    role_arn = "${{ values.role_arn }}"
+    role_arn = "{{ roleArn }}"
   }
 }
+
+# provider "aws" {
+#   region = "us-east-1"
+
+#   assume_role {
+#     role_arn = "${{ values.role_arn }}"
+#   }
+# }
